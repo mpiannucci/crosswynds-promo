2493779e24151fb1a24ce63039e13bd385be29dc
Matthew Iannucci <mpiannucci@gmail.com>
2015-03-03 00:01:04 -0500

    First prototype of new email page

--- templates/__init__.py
+++ templates/__init__.py
@@ -11,15 +11,22 @@ def base (page):
     extend_([u'<html>\n'])
     extend_([u'\n'])
     extend_([u'<head>\n'])
-    extend_([u'    <meta name="viewport" content="width=device-width, initial-scale=1">\n'])
+    extend_([u'    <meta name="viewport" content="width=device-width, initial-scale=1.0">\n'])
     extend_([u'    <title>Crosswynds Traders</title>\n'])
+    extend_([u'    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">\n'])
+    extend_([u'    <link rel="stylesheet" href="/static/styles/promo.css">\n'])
+    extend_([u'    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>\n'])
+    extend_([u'    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>\n'])
+    extend_([u'    <script src="/static/scripts/promo.js"></script>\n'])
     extend_([u'</head>\n'])
     extend_([u'\n'])
     extend_([u'<body>\n'])
-    extend_([u'\n'])
-    extend_([u'<!-- TODO: Make the boilerplate base template -->\n'])
-    extend_([escape_(page, False), u'\n'])
-    extend_([u'\n'])
+    extend_([u'    <div id="contentContainer" class="container-fluid">\n'])
+    extend_([u'        <!-- Create the Main Content layout.-->\n'])
+    extend_([u'        <!-- Get the Main Content from the server -->\n'])
+    extend_([u'        ', escape_(page, False), u'\n'])
+    extend_([u'        <!-- Ends Main Content Layout -->\n'])
+    extend_([u'    </div>\n'])
     extend_([u'</body>\n'])
     extend_([u'\n'])
     extend_([u'</html>\n'])
@@ -30,11 +37,22 @@ base = CompiledTemplate(base, 'templates/base.html')
 join_ = base._join; escape_ = base._escape
 
 # coding: utf-8
-def new():
-    __lineoffset__ = -5
+def new (form):
+    __lineoffset__ = -4
     loop = ForLoop()
     self = TemplateResult(); extend_ = self.extend
-    extend_([u'hello NEW world\n'])
+    extend_([u'\n'])
+    extend_([u'<div class="row">\n'])
+    extend_([u'    <div id="comingSoonHeader" class="col-lg-12 jumbotron comingsoon-jumbotron">\n'])
+    extend_([u'        <!-- Header Image -->\n'])
+    extend_([u'    </div>\n'])
+    extend_([u'    <div id="SignUpArea" class="col-sm-6">\n'])
+    extend_([u'        <div class="container">\n'])
+    extend_([u'            <h3>Sign up early and earn rewards</h3>\n'])
+    extend_([u'            ', escape_(form.render_css(), False), u'\n'])
+    extend_([u'        </div>\n'])
+    extend_([u'    </div>\n'])
+    extend_([u'</div>\n'])
 
     return self
 
--- views/promo.py
+++ views/promo.py
@@ -4,8 +4,22 @@ render = web.render
 
 class New:
     ''' The page shown to new users to prompt a sign up'''
+    email_form = web.form.Form(
+        web.form.Textbox('email', web.form.notnull,
+            size=23,
+            description='',
+            placeholder='Enter your email',
+            class_='form-horizontal email-form'),
+        web.form.Button('Sign Up', class_='btn btn-primary email-form')
+    )
+
     def GET(self):
-        return render.new()
+        form = self.email_form()
+        return render.new(form)
+
+    def POST(self):
+        form = self.email_form()
+        return render.new(form)
 
 class Refer:
     ''' 
