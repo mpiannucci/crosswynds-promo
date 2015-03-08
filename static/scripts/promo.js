6af0fe97bdc769dafe2c58ce1b803f70d725b8d4
Matthew Iannucci <mpiannucci@gmail.com>
2015-03-08 17:41:15 -0400

    Fix input cursor on referral link

--- static/scripts/promo.js
+++ static/scripts/promo.js
@@ -1,1 +1,6 @@
-// NOTHING HERE YET
+/* Promo page specific JS fucntions */
+
+function onReferralURLClick() {
+    var ref_url = document.getElementById('referral_display').value;
+    window.prompt("Copy to clipboard: Ctrl+C, Enter", ref_url);
+}
